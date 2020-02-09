018606a5d6d3177f44a48afa0849fda7711da829
jfactory-es <sp.jfactory@gmail.com>
2020-02-09 17:16:16 +0100

    test: JFactoryPromise

--- test/indexClasses.js
+++ test/indexClasses.js
@@ -1,7 +1,7 @@
 require("./JFactoryTraits.test");
 require("./JFactoryFunction.test");
 require("./JFactoryLogger.test");
-// require("./_JFactoryPromise.test"); // TODO
+require("./JFactoryPromise.test");
 // require("./_JFactoryFetch.test"); // TODO
 require("./JFactoryEvents.test");
 require("./JFactoryEventsDOM.test");
