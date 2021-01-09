2ca867dfb9b0046da9c91b7403e10575f6f95534
jfactory-es <sp.jfactory@gmail.com>
2021-01-09 15:01:12 +0100

    1.7.5

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+/*!
+ * jFactory v1.7.5-beta.0
+ * http://github.com/jfactory-es/jfactory
+ * (c) 2019-2021, Stéphane Plazis, http://github.com/jfactory-es/jfactory/blob/master/LICENSE.txt
+ */
+'use strict';
+
+if (process.env.NODE_ENV === "development") {
+  module.exports = require("./jFactory-devel.umd.js");
+} else {
+  module.exports = require("./jFactory.umd.js");
+}
