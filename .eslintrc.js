bddbdf61d90bb9ab3fa1af02e7d6a4c5a6d7fade
jfactory-es <sp.jfactory@gmail.com>
2024-10-27 03:35:19 +0000

    1.8.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,68 +1,64 @@
 module.exports = {
-
   extends: [
-    "eslint:recommended"
+    'eslint:recommended'
   ],
   ignorePatterns: [
-    "/*",
-    "**/_*",
-    "**/*fork",
-    "!scripts/",
-    "!src/",
-    "!test/**",
-    "!.eslintrc.js"
+    '/*',
+    '**/_*',
+    '!scripts/',
+    '!src/',
+    '!test/',
+    '!.eslintrc.js'
   ],
   env: {
     es6: true
   },
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module"
+    sourceType: 'module'
   },
   rules: {
-    "max-len": ["error", 120],
-    "eol-last": ["error", "never"],
-    quotes: ["error", "double", { avoidEscape: true }],
-    "arrow-parens": ["error", "as-needed"],
-    "quote-props": ["error", "as-needed"],
-    "comma-dangle": ["error", "never"],
-    "no-empty": ["error", { allowEmptyCatch: true }],
-    "space-unary-ops": ["error", { words: false }],
-    "space-infix-ops": "error",
-    "object-curly-spacing": ["error", "always", {
+    'max-len': ['error', 120],
+    'eol-last': ['error', 'never'],
+    quotes: ['error', 'double', { avoidEscape: true }],
+    'arrow-parens': ['error', 'as-needed'],
+    'quote-props': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'never'],
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'space-unary-ops': ['error', { words: false }],
+    'space-infix-ops': 'error',
+    'object-curly-spacing': ['error', 'always', {
       // arraysInObjects: false
     }],
-    "no-extra-parens": ["error", "all", {
+    'no-extra-parens': ['error', 'all', {
       conditionalAssign: false
     }],
-    "comma-spacing": ["error", { before: false, after: true }],
-    "arrow-spacing": "error",
-    "func-call-spacing": "error",
-    "key-spacing": ["error", { afterColon: true }],
-    "keyword-spacing": ["error", { before: true }],
-    "space-before-function-paren": ["error", {
-      anonymous: "never",
-      named: "never",
-      asyncArrow: "always"
+    'comma-spacing': ['error', { before: false, after: true }],
+    'arrow-spacing': 'error',
+    'func-call-spacing': 'error',
+    'key-spacing': ['error', { afterColon: true }],
+    'keyword-spacing': ['error', { before: true }],
+    'space-before-function-paren': ['error', {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always'
     }]
   },
-
   overrides: [
-
     {
-      files: ["*.js"],
+      files: ['*.js', '*.cjs'],
       env: { node: true },
       rules: {
         indent: [
           2, 2, {
             SwitchCase: 1
           }],
-        "no-prototype-builtins": "off"
+        'no-prototype-builtins': 'off',
+        quotes: ['error', 'single', { avoidEscape: true }]
       }
     },
-
     {
-      files: ["src/*.mjs"],
+      files: ['src/*.mjs'],
       env: {
         browser: true
       },
@@ -73,29 +69,27 @@ module.exports = {
           }]
       },
       globals: {
-        globalThis: "readonly", // not yet supported
-        process: "readonly", // only process is required
-        COMPILER_DEV: "readonly",
-        COMPILER_DEBUG: "readonly",
-        COMPILER_CLI: "readonly",
-        jFactoryOverride: "readonly",
-        StackTrace: "readonly"
+        globalThis: 'readonly', // not yet supported
+        process: 'readonly', // only process is required
+        COMPILER_DEV: 'readonly',
+        COMPILER_DEBUG: 'readonly',
+        COMPILER_CLI: 'readonly',
+        jFactoryOverride: 'readonly',
+        StackTrace: 'readonly'
       }
     },
-
     {
-      files: ["test/*.test.js"],
+      files: ['test/**/*.test.js'],
       env: {
         browser: true,
         mocha: true
       },
       rules: {
         indent: [
-          2, 4
+          2, 2
         ],
-        "require-atomic-updates": "off"
+        'require-atomic-updates': 'off'
       }
     }
-
   ]
 };
--- dist/umd/jFactory-devel.umd.js
+++ dist/umd/jFactory-devel.umd.js
@@ -1,5 +1,5 @@
 /*!
- * jFactory-devel v1.8.0-alpha.2 2024-10-24
+ * jFactory-devel v1.8.0-alpha.2 2024-10-27
  * https://github.com/jfactory-es/jfactory
  * (c) 2019-2024 Stephane Plazis <sp.jfactory@gmail.com>
  * License: https://raw.githubusercontent.com/jfactory-es/jfactory/master/LICENSE.txt
